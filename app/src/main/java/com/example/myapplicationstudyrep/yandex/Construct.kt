dbb5e37a5e7662cfb61ac76efc8413003fbebf1e
ASUS <apostol_maxim@mail.ru>
2023-04-25 20:32:16 +0300

    добавил геттер и сеттер

--- app/src/main/java/com/example/myapplicationstudyrep/yandex/Construct.kt
+++ app/src/main/java/com/example/myapplicationstudyrep/yandex/Construct.kt
@@ -1,19 +1,38 @@
 package com.example.myapplicationstudyrep.yandex
 
 
-abstract class Human(val name: String, var age: Int)
+abstract class Human(val name: String, open var age: Int)
 
 interface Printable {
     fun printMe()
 }
 
-class Engineer constructor(nameEngineer: String, ageEngineer: Int) :
+class Engineer constructor(nameEngineer: String = "noname", ageEngineer: Int = 0) :
     Human(age = ageEngineer, name = nameEngineer),
     Printable {
 
-    constructor() : this(nameEngineer = "noname", 0)
-    constructor(ageEngineer: Int) : this(nameEngineer = "noname", ageEngineer = ageEngineer)
-    constructor(nameEngineer: String) : this(nameEngineer = nameEngineer, 10)
+//    constructor() : this(nameEngineer = "noname", 0)
+//    constructor(ageEngineer: Int) : this(nameEngineer = "noname", ageEngineer = ageEngineer)
+//    constructor(nameEngineer: String) : this(nameEngineer = nameEngineer, 10)
+
+
+    override var age: Int = ageEngineer
+        get() = field
+        set(value) {
+            println("before $field and new $value")
+            field = value
+        }
+
+    var salary = 1000
+        get() {
+            println(field)
+            return field
+        }
+        set(value) {
+            println("before $field and new $value")
+            field = value
+        }
+
 
     override fun printMe() {
         println("Hi I am $name and i'm $age and my staus")
@@ -24,17 +43,8 @@ class Engineer constructor(nameEngineer: String, ageEngineer: Int) :
 fun main() {
 
     val human = Engineer("Vova", 22)
-    val human1 = Engineer("Marina", 7)
-    val human2 = Engineer(ageEngineer = 7, nameEngineer = "Fedya")
+    val human1 = Engineer()
+    val human2 = Engineer(ageEngineer = 22)
 
-    val engineer = Engineer()
-    val engineer1 = Engineer(8)
-    val engineer2 = Engineer("8")
-
-
-//    val abstract = Human()
-
-    println( human.age)
-    println(human1.name + human1.age)
-    println(human2.name + human2.age)
+    human.salary = 700
 }
